b66ef89a5843517cf53f438aa48380fa00b93e66
unknown <pabarnard@outlook.com>
2021-01-15 17:24:03 -0800

    Very first commit

---
+++ Algos_01162021.js
@@ -0,0 +1,64 @@
+// Introduction to JavaScript and algorithms - session 1, January 16, 2021
+
+/* JavaScript is used a lot in web pages.  You can do stuff to the webpage after it loads, 
+when you click on a button, etc.  Remember that web pages use HTML, CSS and JavaScript.
+
+An algorithm, often referred to as an algo for short, is a set of instructions that is to be
+executed, or run.  For example, you might write an algorithm to run a bunch of simulations to see
+how often you get 3 heads out of 5 coin flips 100 times.  You could write one to retrieve and search
+for data.  There is no limit to what you can do with algorithms.
+
+For the next four weeks, we will go over the basics of JavaScript and what are called the Basic 13
+algos that you have hopefully nail down quickly by the time you graduate from Coding Dojo.
+*/
+
+/* Introduction to JavaScript:
+Variables:
+- MUST start with a letter
+- Typically written in camelCase (first word not capitalized, each word thereafter capitalized)
+- Can also contain numbers, underscores (_) and dollar signs ($)
+- Cannot use keywords (e.g. "if", "else", "do", "while", "for")
+- Case sensitive (so "M" is different from "m")
+
+Data types:
+- Numbers (2, -5, 2.4, 2e6 [2 million])
+- Strings (3 ways to write them), also concatenate (i.e. put them together)
+- Boolean types (true, false)
+- Basic operations (+, -, *, /, % [modulo])
+- Casting data types ("3"+5 vs 3+"5" vs 3+5)
+
+Logical operators:
+- >, <, >=, <=, ==, !=, &&, ||, "!" (not operator) - also === and !== (won't go over in too much detail)
+
+Printing values:
+- console.log()
+
+Functions:
+- Can accept any number of arguments
+- Can return values
+*/
+
+// Commenting code: done two ways
+// "//" is for commenting code for that line only
+/* This is 
+a block comment, where
+code is ignored between the two pairs
+of symbols */
+
+// For loops
+// Incrementing and decrementing (++, --, +=, -=)
+
+// To do: all of the above, then the first 2 of the basic 13 algos - algos 3-6 next week, when we go over arrays
+// Demonstrate T diagrams
+// Show a few ways to run code
+// Take it nice and slow - you'll get the hang of this soon enough!  Be patient, and don't be afraid to ask!
+// Have Q&A session at end to open up for questions
+
+// Handy references:
+// W3, Mozilla, Python Tutor, Eloquent JavaScript, many others as well
+
+// Basic Algo #1: Print 1-255
+// Print all the integers from 1 to 255.
+
+// Basic Algo #2: Print Sum 0-255
+// Print integers from 0 to 255, and with each integer print the sum so far.
