d875351021e852a65644d6fdb5c7a53b88ad0fa0
unknown <pabarnard@outlook.com>
2021-02-03 19:48:17 -0800

    Updated notes for Feb 6 2021

--- Algos_02062021.js
+++ Algos_02062021.js
@@ -3,3 +3,27 @@
 // Basic Algos #11-13
 // Another Q&A session at the end
 
+// JavaScript object: an object is defined with the curly braces "{" and "}"
+// To the left of the colon is a property name, and to the right is a value, for example: 'myProperty': 25
+// Values can be anything - strings, arrays, numbers, even objects
+
+// To call a property: type varName.propName OR varName['propName']; can chain multiple properties if you have nested objects,
+// e.g. myObj.propertyOne.innerProperty1
+
+// Looping through objects - use a "for-in" loop for an object
+
+// Another way: use a "for-of" loop using Object.keys, which returns an array
+
+// Alternate way: using Object.entries, which returns an array - use "for-of" loop for an array
+
+// Handy method for Objects: objName.hasOwnProperty('propName')
+
+// Basic Algo #11: Square the Values
+// Square each value in a given array, returning that same array with changed values.
+
+// Basic Algo #12: Zero Out Negative Numbers
+// Return the given array, after setting any negative values to zero.
+
+// Basic Algo #13: Shift Array Values
+// Given an array, move all values forward by one index, dropping the first and leaving a '0' value at the end.
+
